706af2d052bc3c34e89397da43bac546dafa1dbf
nishant <nishant@DESKTOP-MI00OOH>
2021-05-06 15:18:43 +0530

    first commit

---
+++ springboot-tester/src/main/java/com/example/demo/SpringbootTesterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTesterApplication.class, args);
+	}
+
+}
